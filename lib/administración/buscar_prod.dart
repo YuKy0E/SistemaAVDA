eb4380271ade897585d93b772b2bbc1aa81652b9
abiudbenitez <abiudmisael10@hotmail.com>
2024-05-13 23:52:59 -0600

    Se capturo el valor del texto en una variable para pasarla como parametro

--- lib/administración/buscar_prod.dart
+++ lib/administración/buscar_prod.dart
@@ -1,9 +1,6 @@
 import 'package:avda/administraci%C3%B3n/dispo_prod.dart';
 import 'package:flutter/material.dart';
 
-
-
-
 // ignore: camel_case_types
 class Buscar_prod extends StatefulWidget {
   const Buscar_prod({super.key});
@@ -14,9 +11,9 @@ class Buscar_prod extends StatefulWidget {
 
 // ignore: camel_case_types
 class _Buscar_prodState extends State<Buscar_prod> {
-   TextEditingController nameController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  String codigoProducto = '';
 
   @override
   Widget build(BuildContext context) {
@@ -40,71 +37,61 @@ class _Buscar_prodState extends State<Buscar_prod> {
           ),
         ),
       ),
-
-
-
-      body: //cuerpo(), 
-      Padding(
+      body: //cuerpo(),
+          Padding(
         padding: const EdgeInsets.all(10),
         child: ListView(
           children: <Widget>[
-            /*Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'ALMACEN',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 188, 119, 209),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-                
             Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Buscar producto',
-                  style: TextStyle(fontSize: 20),
-                  )
-                ),*/
-
-
-            Container(
-              
               padding: const EdgeInsets.all(10),
               child: TextField(
                 controller: nameController,
+                onChanged: (value) {
+                  // Actualiza el valor del código del producto cuando cambia el texto
+                  setState(() {
+                    codigoProducto = value;
+                  });
+                },
                 decoration: InputDecoration(
                   //prefixIcon: const Icon(Icons.lock), // Icono para la contraseña
-                  contentPadding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0), // Ajusta el relleno interno
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 20.0), // Ajusta el relleno interno
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0), // Radio de esquina grande para hacerlo ovalado
+                    borderRadius: BorderRadius.circular(
+                        30.0), // Radio de esquina grande para hacerlo ovalado
                   ),
                   labelText: 'Codigo del producto',
                 ),
               ),
             ),
-            
+
             //controlar altura de separacion entre botones
-            const SizedBox(height: 40, width: 50,), // Añade un espacio entre el campo de texto y el botón
-            Center( // Centro el botón horizontalmente
-            child: Container(
-              width: 250, // Ancho fijo para el contenedor del botón
+            const SizedBox(
+              height: 40,
+              width: 50,
+            ), // Añade un espacio entre el campo de texto y el botón
+            Center(
+              // Centro el botón horizontalmente
+              child: Container(
+                width: 250, // Ancho fijo para el contenedor del botón
                 height: 40,
                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                 child: ElevatedButton(
-                  onPressed: ()=>{
-                            Navigator.push(
-                              context, 
-                              MaterialPageRoute(builder: (context)=>const Dispo_pro())
-                            ),
-                          },
-                          style: ButtonStyle(
-               backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 208, 181, 230)),
-                ),
+                  onPressed: () => {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                Dispo_pro(codigoProducto: codigoProducto))),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 208, 181, 230)),
+                  ),
                   child: const Text(
-                  'Buscar',
-                  style: TextStyle(color: Colors.black, fontSize: 17),
+                    'Buscar',
+                    style: TextStyle(color: Colors.black, fontSize: 17),
                   ),
                 ),
               ),
